3567c7dedef9643f8233262932c947549beb5285
josepfpinto <josepfpinto@gmail.com>
2022-03-26 11:25:23 +0000

    bug fix to wait function and to 'Superb 9.0' score

--- services/new_url.py
+++ services/new_url.py
@@ -14,7 +14,7 @@ def get(dateIn, totalDays, totalAdults, numberRooms):
         endDay = str(dateIn.day + totalDays)
         midURL = webUrl.mid.format(month, startDay, year, month, endDay, year, totalAdults, numberRooms)
         g_driver.google_driver.get(webUrl.start + midURL + webUrl.end)
-        webpage_actions.wait(15, "div.sr_header")
+        webpage_actions.wait(10, 'nav.bui-header__bar')
 
     except (NoSuchElementException, TimeoutException) as error:
         exceptions.simple("Timeout - no page load. Error:", error)
